4064ce8a9d189d40295a8b1e1ab38c0fe4c9b430
TuongNghiBui <camellia.bui95@gmail.com>
2023-07-26 12:04:03 +0200

    styling panier.html

--- Assets/JS/user.js
+++ Assets/JS/user.js
@@ -1,8 +1,5 @@
-let counter = 0;
-
 class User {
     constructor(name, email, password) {
-        this.id = counter++;
         this.name = name;
         this.email = email;
         this.password = password;
